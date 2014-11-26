fba24049986e4bb027ef665c1d799a4dee7f26ff
Pol <polvegrau@gmail.com>
2014-11-26 13:49:46 +0100

    Initial APP

---
+++ app/src/main/java/com/pol/pew/MainPage.java
@@ -0,0 +1,48 @@
+package com.pol.pew;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainPage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void playGame(View view) {
+        Intent i = new Intent(this, Game.class);
+        startActivity(i);
+    }
+
+    public void highScores(View view) {
+        Intent i = new Intent(this, HighScores.class);
+        startActivity(i);
+    }
+}
